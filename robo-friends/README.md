66d80fb54e4bd16af3e92d60cb6be98f36c9c5a4
Ugur YILMAZ <uguryilmaz235@gmail.com>
2020-03-23 16:32:47 +0300

    Error boundary added and readme updated

--- robo-friends/README.md
+++ robo-friends/README.md
@@ -6,4 +6,5 @@ To run the project:</p>
 <li>Clone this repo</li>
 <li>Run <code>npm install or yarn install</code></li>
 <li>Run <code>npm start or yarn start</code></li>
+<li>Hosted on Firebase check <a href="https://robofriends.web.app/" target="_blank">robofriends.web.app</a></li>
 </ol>
--- robo-friends/src/containers/App.js
+++ robo-friends/src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "../containers/App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 /*
 We have App component which has two states
 robots and searchfield
@@ -48,7 +49,9 @@ export class App extends Component {
         <h1 className="center f1">Robo Friends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
